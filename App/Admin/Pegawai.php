1b438ed8d459b7a4a55d429fe6a6343ee62b638c
Dzaky <dzakyfarhan866@gmail.com>
2024-10-05 17:29:41 +0700

    Praktikum 6E - Menggunakan Pewarisan

---
+++ App/Admin/Pegawai.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Admin;
+
+class Pegawai {
+    protected int $nip;
+    protected string $nama;
+    private int $no_hp;
+    protected string $alamat;
+
+    public function __construct(int $nip, string $nama, int $no_hp, string $alamat) {
+        $this->nip = $nip;
+        $this->nama = $nama;
+        $this->no_hp = $no_hp;
+        $this->alamat = $alamat;
+    }
+
+    public function cekIn(): bool {
+        return true; 
+    }
+
+    public function cekOut(): bool {
+        return true;
+    }
+
+    public function getNoHp(): int {
+        return $this->no_hp;
+    }
+
+    public function setNoHp(int $no_hp): void {
+        $this->no_hp = $no_hp;
+    }
+}
+
---
+++ App/Admin/TenagaKependidikan.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Admin;
+
+class TenagaKependidikan extends Pegawai {
+    private int $gaji_pokok;
+
+    public function __construct(int $nip, string $nama, int $no_hp, string $alamat, int $gaji_pokok) {
+        parent::__construct($nip, $nama, $no_hp, $alamat);
+        $this->gaji_pokok = $gaji_pokok;
+    }
+
+    public function cuti(): void {
+        echo "{$this->nama} mengambil cuti.";
+    }
+}
+
+?>
